fb4e836806017117a39723fe950893069d171e49
IgnacioCCM <carlos.campos@tectijuana.edu.mx>
2021-06-10 18:23:44 -0700

    Fitting Naive Bayes to the train

--- unit3/Evaluative/Evaluative_Practice.r
+++ unit3/Evaluative/Evaluative_Practice.r
@@ -16,3 +16,10 @@ test_set = subset(dataset, split == FALSE)
 
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
+
+#install.packages('e1071')
+library(e1071)
+classifier = naiveBayes(formula = Purchased ~ .,
+                        data = training_set,
+                        type = 'C-classification',
+                        kernel = 'linear')
