155994f07a620ba48d6c4f87b708c32fd5ac75f2
IgnacioCCM <carlos.campos@tectijuana.edu.mx>
2021-06-10 18:18:13 -0700

    Splitting the dataset

--- unit3/Evaluative/Evaluative_Practice.r
+++ unit3/Evaluative/Evaluative_Practice.r
@@ -6,3 +6,10 @@ dataset = read.csv(file.choose())
 dataset = dataset[3:5]
 ###we pushed data factor 
 dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
